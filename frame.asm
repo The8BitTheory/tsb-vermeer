f436670fd9ef4069a40efa23682364ed34b626ed
The8BitTheory <the8bittheory@gmail.com>
2025-08-21 17:14:07 +0200

    Frame ML routine

---
+++ frame.asm
@@ -0,0 +1,230 @@
+;6502 assembly
+
+*= $c000
+
+;!to "frame.bin.prg"
+
+!ct pet
+
+fr          =   1   ;frame index. needs to be parsed from SYS parameter
+
+; y=1,x=1,w=26,h=3
+zeileanf    =   $C5DF
+spalteanf   =   $C5E0
+spaltenanz  =   $C5E1
+zeilenanz   =   $C5E2
+
+basromaus   =   $8e5f
+basromein   =   $8e3a
+;l20pl40     =   $8dd6
+
+mve9a       =   $9ecb
+
+mkbox       =   $a158
+;mkline      =   $a052
+;fillmt      =   $a084
+
+; calculate absolute address of frame index offsets
+    clc
+    lda fa
+    adc #fr
+    adc #fr
+    sta f2
+    lda fa+1
+    adc #0
+    sta f2+1
+
+; calculate layout data address by reading the offset and then adding it to fa
+    ldy #0
+    lda (f2),y
+    tax
+    iny
+    lda (f2),y
+    pha
+    iny
+    clc
+    txa
+    adc fa
+    sta f2
+    pla
+    adc fa+1
+    sta f2+1
+
+; get frame coordinates
+    ldy #0
+    jsr loadCoordinates ;increases y by 2
+    lda (f2),y
+    sta spaltenanz
+    iny
+    lda (f2),y
+    sta zeilenanz
+
+    ; zeile mit rahmendaten vorbereiten
+    ; $0137 für rahmenzeile (BD$). folgende Zeichen umgekehrt 111,183,112,180,32,170,108,187,188
+    ; $0b für länge (8 zeichen)
+    ; write color 6 ($a6 evtl nur positiv, wert für color-ram evtl in $20?)
+;    ldx #fb
+;    stx $a6
+
+    ldx #8
+    stx $0b
+
+-   lda border,x
+    sta $0137,x
+    dex
+    bpl -
+
+    jsr basromaus
+    
+    ; screen-ram work for box
+    jsr mkbox
+    
+    ; color-ram work for box (replicates mve9 from TSB's INSERT routine)
+    inc $a6    
+    lda #fb
+    jsr mve9a
+    
+    jsr basromein
+
+    ; call INSERT BD$,BY,BX,BW,BH,#FB
+
+    ; set shadow-color
+    ;ldx #sb
+    ;stx $a6
+    
+    rts
+
+    ;inc zeileanf
+    ;inc spalteanf
+    ;dec spaltenanz
+    ;dec spaltenanz
+    ;dec zeilenanz
+    ;dec zeilenanz
+    ; call FILL BY+1,BX+1,BW-2,BH-2,32,1
+    ; jsr mve9
+
+    ; call FCOL BY+BH,BX+1,BW,1,#sb
+    ; jsr mve3
+    ; call FCOL BY+1,BX+BW,1,BH,#sb
+
+    ; increase layout data offset by 4 (that's how long data for a frame is)
+incBy4
+    clc
+    lda f2
+    adc #4
+    sta f2
+    bcc checkNext
+    inc f2+1
+
+; check next type byte
+; 0 = done
+; 1 = cpr
+; 2 = vpr
+; 3 = vus
+checkNext
+    ldy #0
+    lda (f2),y
+
+    beq .done   ; zero means no more layout data for this frame
+    iny
+
+    cmp #1
+    beq cpr
+
+    cmp #2
+    beq vpr
+
+    cmp #3
+    beq vus
+
+    ; invalid value. set carry flag and leave (that shows something went wrong)
+    sec
+    rts
+
+; PRINT AT(). prints from the textconsts file with a constant index to constant coordinates
+cpr
+    jsr loadCoordinates
+    
+    lda (f2),y  ;load index of text constant
+    sta tx
+
+    ; jump to constant print handling (same as for vpr)
+
+    jmp incBy4
+
+; PRINT AT().  prints from the textconsts file with a variable index (via param) to constant coordinates
+vpr
+    jsr loadCoordinates
+
+    lda (f2),y  ;load index of variable-array
+    sta vx
+
+    ; iterate variable array to given index and extract the value into tx
+    ; sta tx
+
+    ; create temporary string descriptor that points into text constants
+
+    ; calculate absolute y and x positions (framepos + itempos)
+    ; poke length into MP$ descriptor
+    ; PT=TC+TX*3:TL=PEEK(PT):POKEMA,TL
+    ; write value from textconsts at tx into MP$
+    ;D!POKE$5A,TC+D!PEEK(PT+1):D!POKE$58,MP:POKE781,1:POKE782,TL:SYS $A3EC
+
+    ;PRINT AT(BY+CY,BX+CX) MP$;
+    
+
+    jmp incBy4
+
+; USE AT(). prints a numeric value from VX() with a given CF$ index
+vus
+    jsr loadCoordinates
+
+    lda (f2),y
+    sta cf
+    iny
+    lda (f2),y
+    sta vx
+
+    ; iterate variable array to given index and extract the  value 
+
+    ; calculate absolute y and x positions (framepos + itempos)
+
+    ; get correct CF$ entry
+
+    ;USE AT(BY+CY,BX+CX) CF$(CF),VX(VX)
+    
+    clc
+    lda f2
+    adc #5
+    sta f2
+    bcc +
+    inc f2+1
++   jmp checkNext
+
+
+; we're done. clear carry (to indicate that everything worked out fine) and return
+.done
+    clc
+    rts
+
+loadCoordinates
+    lda (f2),y
+    sta zeileanf
+    iny
+    lda (f2),y
+    sta spalteanf
+    iny
+    rts
+
+fa !word $7c00 ;address where the binary frame data is stored. either parse from SYS or POKE
+;f2  !word 0 ;absolute address pointing to the offset of the layout data
+f2 = $fb
+fb = 6 ; foreground border
+sb = 11; shadow border
+
+tx !byte 0
+cf !byte 0
+vx !byte 0
+
+border !byte 188,187,108,170,32,180,112,183,111
+;border !byte 111,183,112,180,32,170,108,187,188
