bd035e01dfe64aa1c41d42bce3a6a1d3e64589dc
The8BitTheory <the8bittheory@gmail.com>
2025-08-22 09:21:57 +0200

    drawing frame now works

--- frame.asm
+++ frame.asm
@@ -8,6 +8,7 @@
 
 fr          =   1   ;frame index. needs to be parsed from SYS parameter
 
+helpvec     =   $b0
 ; y=1,x=1,w=26,h=3
 zeileanf    =   $C5DF
 spalteanf   =   $C5E0
@@ -21,6 +22,7 @@ basromein   =   $8e3a
 mve9a       =   $9ecb
 
 mkbox       =   $a158
+movez       =   $a0b1
 ;mkline      =   $a052
 ;fillmt      =   $a084
 
@@ -55,9 +57,11 @@ mkbox       =   $a158
     jsr loadCoordinates ;increases y by 2
     lda (f2),y
     sta spaltenanz
+    sta tempW
     iny
     lda (f2),y
     sta zeilenanz
+    sta tempH
 
     ; zeile mit rahmendaten vorbereiten
     ; $0137 für rahmenzeile (BD$). folgende Zeichen umgekehrt 111,183,112,180,32,170,108,187,188
@@ -67,7 +71,7 @@ mkbox       =   $a158
 ;    stx $a6
 
     ldx #8
-    stx $0b
+    stx helpvec
 
 -   lda border,x
     sta $0137,x
@@ -76,36 +80,112 @@ mkbox       =   $a158
 
     jsr basromaus
     
+    ; call INSERT BD$,BY,BX,BW,BH,#FB
+    ; compute address (parameters are set)
+    lda #0
+    sta $a6
+    jsr movez
+    
     ; screen-ram work for box
     jsr mkbox
     
     ; color-ram work for box (replicates mve9 from TSB's INSERT routine)
-    inc $a6    
+    inc $a6
+    jsr movez
+    
     lda #fb
     jsr mve9a
+
+    ; call FILL BY+1,BX+1,BW-2,BH-2,32,1
+    inc zeileanf
+    inc spalteanf
+    dec spaltenanz
+    dec spaltenanz
+    dec zeilenanz
+    dec zeilenanz
+
+    ; calculate fill dimensions for screen-ram
+    lda #0
+    sta $a6
+    jsr movez
     
+    ; set fill character (space)
+    lda #32
+    ; write to screen-ram
+    jsr mve9a
+    
+    ; calculate fill dimensions for color-ram
+    inc $a6
+    jsr movez
+
+    ; set foreground-color to fill
+    lda #1
+    ; write to color-ram
+    jsr mve9a
+    
+    
+    ; horizontal shadow below frame
+    ; call FCOL BY+BH,BX+1,BW,1,#sb
+    clc
+    lda tempY         ;load y
+    adc tempH         ;add height
+    sta zeileanf      ;store y
+    
+    clc
+    lda tempX         ;load x
+    adc #1            ;add 1
+    sta spalteanf     ;store x
+    
+    clc
+    lda tempW         ;load width
+    sta spaltenanz    ;store width
+    
+    clc
+    lda #1            ;load height
+    sta zeilenanz     ;store height
+    
+    ; calc addresses for color-ram
+    inc $a6
+    jsr movez
+    
+    ; write color value to calculated addresses
+    lda #sb
+    jsr mve9a
+    
+    ; vertical shadow right of frame
+    ; call FCOL BY+1,BX+BW,1,BH,#sb
+    clc
+    lda tempY         ;load y
+    adc #1            ;add 1
+    sta zeileanf      ;store y
+    
+    clc
+    lda tempX         ;load x
+    adc tempW         ;add width
+    sta spalteanf     ;store x
+    
+    clc
+    lda #1            ;load 1
+    sta spaltenanz    ;store width
+    
+    clc
+    lda tempH         ;load height
+    sta zeilenanz     ;store height
+    
+    ; calc addresses for color-ram
+    inc $a6
+    jsr movez
+    
+    ; write color value to calculated addresses
+    lda #sb
+    jsr mve9a
+
+
     jsr basromein
-
-    ; call INSERT BD$,BY,BX,BW,BH,#FB
-
-    ; set shadow-color
-    ;ldx #sb
-    ;stx $a6
     
     rts
 
-    ;inc zeileanf
-    ;inc spalteanf
-    ;dec spaltenanz
-    ;dec spaltenanz
-    ;dec zeilenanz
-    ;dec zeilenanz
-    ; call FILL BY+1,BX+1,BW-2,BH-2,32,1
-    ; jsr mve9
-
-    ; call FCOL BY+BH,BX+1,BW,1,#sb
-    ; jsr mve3
-    ; call FCOL BY+1,BX+BW,1,BH,#sb
+    
 
     ; increase layout data offset by 4 (that's how long data for a frame is)
 incBy4
@@ -210,9 +290,11 @@ vus
 loadCoordinates
     lda (f2),y
     sta zeileanf
+    sta tempY
     iny
     lda (f2),y
     sta spalteanf
+    sta tempX
     iny
     rts
 
@@ -221,6 +303,12 @@ fa !word $7c00 ;address where the binary frame data is stored. either parse from
 f2 = $fb
 fb = 6 ; foreground border
 sb = 11; shadow border
+
+; stores x,y,w,h because we need them several times
+tempY  !byte 0
+tempX  !byte 0
+tempW  !byte 0
+tempH  !byte 0
 
 tx !byte 0
 cf !byte 0
