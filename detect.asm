a9805924e8ffe91c81bb18ff5b09e44eb30323cb
The8BitTheory <the8bittheory@gmail.com>
2025-08-31 11:45:17 +0200

    added reu and mega65 dma detection

---
+++ detect.asm
@@ -0,0 +1,64 @@
+*= $c000
+
+REUSTATUS           = $df00         ;Statusregister (nur lesen, wird dann gelöscht!)
+REUCOMMAND          = $df01         ;Befehlsregister
+
+
+jmp checkForREU
+jmp checkForMega65
+
+
+;*******************************************************************************
+;*** Prüfen, ob eine REU angeschlossen ist. 
+;*** Um auch die 1700 zu erkennen wird NICHT so, wie auf der 1764 Demo-Disk mit
+;*** #$00 geprüft. Bei der 1700 kann $df00 den Wert #$00 annehmen!!
+;*******************************************************************************
+;*** Übergabe: -
+;*******************************************************************************
+;*** Rückgabe: Y = 0 REU vorhanden, sonst nicht!
+;*******************************************************************************
+;*** ändert  : A, X, Y, SR
+;*******************************************************************************
+checkForREU
+  ldy #$ff                           ;Erstmal von keiner REU ausgehen
+  lda REUSTATUS                      ;Status-Register lesen, um Bit 7-5 zu löschen
+  sty REUSTATUS                      ;schreiben wir zum Test mal #$ff hinein
+  cpy REUSTATUS                      ;und schauen, ob der Wert erhalten bleibt
+  beq .exit                          ;wenn JA, KEINE REU!!!
+  ldx #$04                           ;Wir testen nur Register 2-5 
+.loop
+  txa
+  sta REUCOMMAND,X                   ;Testwert speichern
+  cmp REUCOMMAND,X                   ;gleicher Wert?
+  bne .exit                          ;wenn nicht, KEINE REU!!!
+  dex                                ;Schleifenzähler verringern
+  bne .loop                          ;wenn > 0, nochmal
+  ldy #$00                           ;sonst wurde eine REU gefunden
+.exit
+  rts                                ;zurück
+ 
+
+; Y=0 VIC-IV erkannt, Mega65
+checkForMega65
+  ldy #$ff
+  
+  lda #1
+  sta $d000
+  
+  lda #$47
+  sta $d02f
+  
+  lda #$53
+  sta $d02f
+  
+  lda #0
+  sta $d100
+  
+  lda $d000
+  cmp #1
+  bne .exit
+  
+  
+  
+  ldy #0
+  rts
